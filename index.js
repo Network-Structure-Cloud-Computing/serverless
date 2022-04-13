95122b2d2fa980907eec8ff6b4c3b5571805a018
yj199522 <jain.yash3@northeastern.edu>
2022-04-13 13:06:03 -0400

    Added

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ var sendEmail = (data) => {
 
     let link = `http://${data.domainName}/v1/verifyUserEmail?email=${data.username}&token=${data.token}`;
 
-    let body = "Hi "+ data.first_name +" ,\n\n"+
-    "Your profile has been created successfully on our application. You need to verify your email address before using your account by clicking on this link:" +"\n"+ link + "\n\n\n"+
-    "Thanks & Regards,\n noreply@"+data.domainName
+    // let body = "Hi "+ data.first_name +" ,\n\n"+
+    // "Your profile has been created successfully on our application. You need to verify your email address before using your account by clicking on this link:" +"\n"+ link + "\n\n\n"+
+    // "Thanks & Regards,\n noreply@"+data.domainName
+    let body = `<html><body>Hi ${data.first_name} ${data.lastName},<br> Your profile has been created successfully on our application. You need to verify your email address before using your account by clicking on this link: <br><br><a href=${link}>Verify your account</a><br><br><br> Kind Regards,<br> <strong>noreply@${data.domainName}<strong></body></html>`
     let from = "noreply@"+data.domainName
     let emailBody = {
         Destination: {
