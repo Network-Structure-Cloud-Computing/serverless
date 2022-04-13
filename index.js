6cdf45f20f17a3ddacccfa19d05a1328fc96dbef
yj199522 <jain.yash3@northeastern.edu>
2022-04-13 13:12:50 -0400

    Added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var sendEmail = (data) => {
     // let body = "Hi "+ data.first_name +" ,\n\n"+
     // "Your profile has been created successfully on our application. You need to verify your email address before using your account by clicking on this link:" +"\n"+ link + "\n\n\n"+
     // "Thanks & Regards,\n noreply@"+data.domainName
-    let body = `<html><body>Hi ${data.first_name} ${data.lastName},<br> Your profile has been created successfully on our application. You need to verify your email address before using your account by clicking on this link: <br><br><a href=${link}>Verify your account</a><br><br><br> Kind Regards,<br> <strong>noreply@${data.domainName}<strong></body></html>`
+    let body = `<html><body>Hi ${data.first_name},<br> Your profile has been created successfully on our application. You need to verify your email address before using your account by clicking on this link: <br><br><a href=${link}>Verify your account</a><br><br><br> Kind Regards,<br> <strong>noreply@${data.domainName}<strong></body></html>`
     let from = "noreply@"+data.domainName
     let emailBody = {
         Destination: {
@@ -24,7 +24,7 @@ var sendEmail = (data) => {
         },
         Message: {
             Body: {
-                Text: { Data: body },
+                Html: { Data: body },
             },
             Subject: { Data: "User Account Verification Email" },
         },
